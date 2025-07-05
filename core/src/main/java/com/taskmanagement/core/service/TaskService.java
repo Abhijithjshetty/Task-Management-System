8a98106bea24e215d42eb188664f2cc2d7ed6ea1
abhi <abhijith.shetty@tifantsingress.com>
2025-07-05 19:17:18 +0530

    backend completed

--- core/src/main/java/com/taskmanagement/core/service/TaskService.java
+++ core/src/main/java/com/taskmanagement/core/service/TaskService.java
@@ -17,8 +17,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/api/v1/task")
 @Slf4j
